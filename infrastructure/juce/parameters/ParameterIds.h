4c95f25979767fb51ba8d760799b7d443ac6f9e2
Achilles Charmpilas <achillefs.charmpilas@toptal.com>
2026-05-06 14:27:57 +0300

    style: fix clang-format alignment in ParameterIds.h

--- infrastructure/juce/parameters/ParameterIds.h
+++ infrastructure/juce/parameters/ParameterIds.h
@@ -2,15 +2,15 @@
 
 namespace vp330::params {
 
-constexpr auto kChoirLowerMale8   = "choir_lower_male_8";
-constexpr auto kChoirLowerMale4   = "choir_lower_male_4";
-constexpr auto kChoirUpperMale8   = "choir_upper_male_8";
+constexpr auto kChoirLowerMale8 = "choir_lower_male_8";
+constexpr auto kChoirLowerMale4 = "choir_lower_male_4";
+constexpr auto kChoirUpperMale8 = "choir_upper_male_8";
 constexpr auto kChoirUpperFemale4 = "choir_upper_female_4";
-constexpr auto kEnsembleEnabled   = "ensemble_enabled";
-constexpr auto kVibratoRate       = "vibrato_rate";
-constexpr auto kVibratoDepth      = "vibrato_depth";
-constexpr auto kAttack            = "attack";
-constexpr auto kRelease           = "release";
-constexpr auto kOutputLevel       = "output_level";
+constexpr auto kEnsembleEnabled = "ensemble_enabled";
+constexpr auto kVibratoRate = "vibrato_rate";
+constexpr auto kVibratoDepth = "vibrato_depth";
+constexpr auto kAttack = "attack";
+constexpr auto kRelease = "release";
+constexpr auto kOutputLevel = "output_level";
 
 } // namespace vp330::params
